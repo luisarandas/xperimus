4b8ae16638953deb182ba3a9400b7fe71ce13ffd
Luis Henrique Arandas <luis.arandas@gmail.com>
2019-12-12 21:25:52 +0000

    add login auth message cover and rooms

--- app.py
+++ app.py
@@ -1,23 +1,93 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    login_required,
+    logout_user,
+)
+
+from passlib.hash import pbkdf2_sha256
+
 from wtform_fields import *
+from models import *
 
 # Configure app
 app = Flask(__name__)
 app.secret_key = "replace later"
+
+# Configure database
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgres://eajdbtaoaffwhp:c9e4b7ffa763731fffdd0af7ccd9c9888d78d1c29f1664d582d1a885e99779fa@ec2-54-217-243-19.eu-west-1.compute.amazonaws.com:5432/d8eaj48551fat3"
+db = SQLAlchemy(app)
+
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     reg_form = RegistrationForm()
+
+    # Updated database if validation success
+
     if reg_form.validate_on_submit():
-        return "Great success!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        # Hash password
+        hashed_pswd = pbkdf2_sha256.hash(password)
+
+        # Add user to DB
+        user = User(username=username, password=hashed_pswd)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("login"))
 
     return render_template("index.html", form=reg_form)
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    login_form = LoginForm()
+
+    # Allow user to login if validation success
+
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for("chat"))
+
+    return render_template("login.html", form=login_form)
+
+
+@app.route("/chat", methods=["GET", "POST"])
+# @login_required
+def chat():
+    if not current_user.is_authenticated:
+        return "Please login before acessing chat"
+
+    return "Chat with me"
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    logout_user()
+    return "Logged out using flask-login!"
+
+
 if __name__ == "__main__":
-    # will always execute
+    # Will always execute
     app.run(debug=True)
 
 # WSGI interface for backend
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,6 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from passlib.hash import pbkdf2_sha256
+from models import User
+
+
+def invalid_credentials(form, field):
+    """ Username and password checker """
+    username_entered = form.username.data
+    password_entered = field.data
+
+    # Check credentials are valid
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect.")
+    elif not pbkdf2_sha256.verify(password_entered, user_object.password):
+        raise ValidationError("Username or password is incorrect.")
 
 
 class RegistrationForm(FlaskForm):
@@ -28,3 +43,23 @@ class RegistrationForm(FlaskForm):
         ],
     )
     submit_button = SubmitField("Create")
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError(
+                "Username already exists. Select a different username."
+            )
+
+
+class LoginForm(FlaskForm):
+    """ Login Form """
+
+    username = StringField(
+        "username_label", validators=[InputRequired(message="Username required")]
+    )
+    password = PasswordField(
+        "password_label",
+        validators=[InputRequired(message="Password required"), invalid_credentials],
+    )
+    submit_button = SubmitField("Login")
