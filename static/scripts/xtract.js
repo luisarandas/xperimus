1ea4a614462f57b69d917734507de0f59dd7d73c
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-01-25 14:50:56 +0000

    transfer learning example

--- static/scripts/xtract.js
+++ static/scripts/xtract.js
@@ -14,6 +14,7 @@
 // https://www.audiolabs-erlangen.de/fau/professor/mueller/demos
 // https://github.com/0xfe/vexflow
 // https://github.com/borismus/spectrogram
+// https://codelabs.developers.google.com/codelabs/tensorflowjs-audio-codelab/index.html#5
 
 class xperimusFeatureCollector {
 
@@ -728,7 +729,7 @@ https://github.com/googlecreativelab/teachablemachine-community/tree/master/libr
 let recognizer;
 
 
-function predictWord() {
+/*function predictWord() {
  // Array of words that the recognizer is trained to recognize.
  const words = recognizer.wordLabels();
  console.log(recognizer);
@@ -738,13 +739,101 @@ function predictWord() {
    // Find the most probable word.
    scores.sort((s1, s2) => s2.score - s1.score);
    document.querySelector('#console').textContent = scores[0].word;
- }, {probabilityThreshold: 0.75});
-}
+ }, {probabilityThreshold: 0.75}); // when the model fires if thinks +75%
+}*/
 
 async function app() {
  recognizer = speechCommands.create('BROWSER_FFT');
  await recognizer.ensureModelLoaded();
- predictWord();
+ // predictWord();
+ buildModel();
 }
 
 app();
+
+// One frame is ~23ms of audio.
+const NUM_FRAMES = 3;
+let examples = [];
+
+function collect(label) {
+    if (recognizer.isListening()) {
+        return recognizer.stopListening();
+    }
+    if (label == null) {
+        return;
+    }
+    recognizer.listen(async ({spectrogram: {frameSize, data}}) => {
+        let vals = normalize(data.subarray(-frameSize * NUM_FRAMES));
+        examples.push({vals, label});
+        document.querySelector('#console').textContent =
+            `${examples.length} examples collected`;
+    }, {
+        overlapFactor: 0.999,
+        includeSpectrogram: true,
+        invokeCallbackOnNoiseAndUnknown: true
+    });
+}
+function normalize(x) {
+    const mean = -100;
+    const std = 10;
+    return x.map(x => (x - mean) / std);
+}
+
+// The model has 4 layers: a convolutional layer that processes the audio data 
+// (represented as a spectrogram), a max pool layer, a flatten layer, and a dense 
+// layer that maps to the 3 actions
+
+const INPUT_SHAPE = [NUM_FRAMES, 232, 1]; // where each frame is 23ms of audio containing 232 numbers (buckets to capture human voice -> frequencies -> change for piano)
+let model;
+
+async function train() {
+    toggleButtons(false);
+    const ys = tf.oneHot(examples.map(e => e.label), 3);
+    const xsShape = [examples.length, ...INPUT_SHAPE];
+    const xs = tf.tensor(flatten(examples.map(e => e.vals)), xsShape);
+
+    await model.fit(xs, ys, {
+        batchSize: 16,
+        epochs: 10,
+        callbacks: {
+            onEpochEnd: (epoch, logs) => {
+                document.querySelector('#console').textContent = 
+                `Accuracy: ${(logs.acc * 100).toFixed(1)}% Epoch: ${epoch +1}`;
+            }
+        }
+    });
+    tf.dispose([xs, ys]);
+    toggleButtons(true);
+}
+
+function buildModel() {
+    model = tf.sequential();
+    model.add(tf.layers.depthwiseConv2d({
+        depthMultiplier: 8,
+        kernelSize: [NUM_FRAMES, 3],
+        activation: 'relu',
+        inputShape: INPUT_SHAPE
+    }));
+    model.add(tf.layers.maxPooling2d({poolSize: [1, 2], strides: [2, 2]}));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({units: 3, activation: 'softmax'}));
+
+    const optimizer = tf.train.adam(0.01);
+    model.compile({
+        optimizer,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    });
+}
+function toggleButtons(enable) {
+    document.querySelectorAll('button').forEach(b => b.disabled = !enable);
+}
+
+function flatten(tensors) {
+    const size = tensors[0].length;
+    const result = new Float32Array(tensors.length * size);
+    tensors.forEach((arr, i) => result.set(arr, i * size));
+    return result;
+}
+// should end this
+// https://codelabs.developers.google.com/codelabs/tensorflowjs-audio-codelab/index.html#7
