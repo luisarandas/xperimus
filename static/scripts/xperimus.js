7c0e0c7e6f4afca993289a4d7685ba31afaee3bc
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-02-05 12:55:12 +0000

    back on track with instantiated objects

--- static/scripts/xperimus.js
+++ static/scripts/xperimus.js
@@ -1,6 +1,37 @@
-console.log("hello");
 
-import {sayHi} from './sayHi.js';
+// Instantiate Model
+
+const tensorflow = tf;
+const SpeechCommands = speechCommands;
+
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+
+const transferModelNameInput = document.getElementById('transfer-model-name');
 
 
-console.log("well");
+const XFER_MODEL_NAME = 'xfer-model';
+const MIN_EXAMPLES_PER_CLASS = 8;
+
+let recognizer;
+let transferWords;
+let transferRecognizer;
+let transferDurationMultiplier;
+
+(async function() {
+    console.log("Creating Recognizer");
+    recognizer = SpeechCommands.create('BROWSER_FFT');
+
+    // await populateSavedTransferModelsSelect();
+    // onde ele vem por HTTP
+
+    recognizer.ensureModelLoaded().then(() => {
+        console.log("batatas");
+        startButton.disabled = false;
+        transferModelNameInput.value = `model-${getDateString()}`;
+    });
+
+})();
+
+console.log(tensorflow);
+console.log(SpeechCommands);
